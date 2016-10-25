a7b38da4e66da7c248f9295b79e41771e76a4e41
oradistrict.com <oradistrict@users.noreply.github.com>
2016-10-25 14:35:14 +0200

    PK: simple config for sql*plus

---
+++ login.sql
@@ -0,0 +1,7 @@
+set termout off
+set lines 92
+set pages 250
+alter session set nls_date_format = 'YYYY-MM-DD HH24:MI:SS';
+alter session set nls_timestamp_format = 'YYYY-MM-DD HH24:MI:SS.FF';
+alter session set nls_timestamp_tz_format = 'YYYY-MM-DD HH24:MI:SS.FF TZH:TZM';
+set termout on
